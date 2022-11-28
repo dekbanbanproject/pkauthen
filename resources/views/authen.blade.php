337bb3ad1d94fe0478a575a849c4d6204b5e82ad
dekbanbanproject <dekbanbanproject@gmail.com>
2022-11-28 13:52:09 +0700

    นน

--- resources/views/authen.blade.php
+++ resources/views/authen.blade.php
@@ -39,8 +39,8 @@
             <div class="card shadow-lg">
                 <div class="card-header text-center">
                     <img src="{{ asset('images/spsch.jpg') }}" alt="Image" class="img-thumbnail" width="600px" height="130px">
-                    <img src="{{ asset('images/dataaudit.jpg') }}" alt="Image" class="img-thumbnail" width="135px" height="135px">
-                    {{-- <img src="{{ asset('images/logo150.png') }}" alt="Image" class="img-thumbnail" width="135px" height="135px"> --}}
+                    {{-- <img src="{{ asset('images/dataaudit.jpg') }}" alt="Image" class="img-thumbnail" width="135px" height="135px"> --}}
+                    <img src="{{ asset('images/logo150.png') }}" alt="Image" class="img-thumbnail" width="135px" height="135px">
                 </div>
                 <div class="card-body">
                     <form action="{{ route('authencode') }}" method="POST" id="insert_AuthencodeForm">
